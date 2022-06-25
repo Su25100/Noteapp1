f2a65cb20d0843d44d5ff701ac23e7a96b216bdd
su25100 <surbhibanke2599@gmail.com>
2022-06-26 01:15:52 +0530

    first coommit

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const getNotes = require('./notes.js');
+ //const getNotes = require('./notes.js');
 
-//const fs = require('fs');
-//fs.appendFileSync('notes.txt', ' vroom');
+ //const fs = require('fs');
+ //fs.appendFileSync('notes.txt', ' vroom');
 
-//const sum = add(4, -2)
-//console.log(sum);
+ //const sum = add(4, -2)
+ //console.log(sum);
 
-const msg = getNotes()
+ /*const msg = getNotes()
 
-console.log(msg)
+ console.log(msg)
+ */
+
+ const validator = require('validator')
+ const getNotes = require('./notes.js')
+
+ const msg = getNotes()
+ console.log(msg)
+
+ //isEmail: checks the valid email address
+
+ //isURL : checks for the valid url
+ console.log(validator.isURL(('https://www.npmjs.com/package/validator')))
