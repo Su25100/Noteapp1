f2692ca7eab54462ae24ae6a30f91820644dd50b
su25100 <surbhibanke2599@gmail.com>
2022-06-26 14:39:00 +0530

    first coommit

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
- //const getNotes = require('./notes.js');
-
- //const fs = require('fs');
- //fs.appendFileSync('notes.txt', ' vroom');
-
- //const sum = add(4, -2)
- //console.log(sum);
-
- /*const msg = getNotes()
-
- console.log(msg)
- */
- /*install color module to print text in color specified
- use--> npm install colors
- then define the colors
- */
- const colors = require('colors')
+ const chalk = require('chalk')
  const validator = require('validator')
  const getNotes = require('./notes.js')
 
  const msg = getNotes()
  console.log(msg)
 
- //isEmail: checks the valid email address
-
- //isURL : checks for the valid url
- console.log(validator.isURL(('https://www.npmjs.com/package/validator')))
-
- console.log('success'.green.bold); // outputs green text
+ const greenmsg = chalk.blue.inverse.bold('Success!')
+ console.log(greenmsg)
+ console.log(process.argv[0])
